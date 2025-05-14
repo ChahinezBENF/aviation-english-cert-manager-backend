871ba8b1fcce25f7e0c06ada859b885f9dac0284
ChahinezBENF <Chahinez-benfriha27@outlook.com>
2025-05-14 16:09:33 -0400

    12 th commit

--- routes/airport.js
+++ routes/airport.js
@@ -5,17 +5,17 @@ const authenticateToken = require('../middleware/authenticateToken');
 const authorizeRoles = require('../middleware/authorizeRoles');
 
 // Routes for airports with Authen and Autor
-router.get('/', authenticateToken, airportController.getAllAirports);
-router.post('/', authenticateToken, authorizeRoles('hr'), airportController.createAirport);
-router.get('/:id', authenticateToken, airportController.getAirportById);
-router.put('/:id', authenticateToken, authorizeRoles('hr'), airportController.updateAirport);
-router.delete('/:id', authenticateToken, authorizeRoles('hr'), airportController.deleteAirport);
+// router.get('/', authenticateToken, airportController.getAllAirports);
+// router.post('/', authenticateToken, authorizeRoles('hr'), airportController.createAirport);
+// router.get('/:id', authenticateToken, airportController.getAirportById);
+// router.put('/:id', authenticateToken, authorizeRoles('hr'), airportController.updateAirport);
+// router.delete('/:id', authenticateToken, authorizeRoles('hr'), airportController.deleteAirport);
 
 // Routes for airports 
-// router.get('/',  airportController.getAllAirports);
-// router.post('/',  airportController.createAirport);
-// router.get('/:id',  airportController.getAirportById);
-// router.put('/:id',  airportController.updateAirport);
-// router.delete('/:id',  airportController.deleteAirport);
+router.get('/',  airportController.getAllAirports);
+router.post('/',  airportController.createAirport);
+router.get('/:id',  airportController.getAirportById);
+router.put('/:id',  airportController.updateAirport);
+router.delete('/:id',  airportController.deleteAirport);
 
 module.exports = router;
