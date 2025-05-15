49a61a708d0fcd7992ecaf4fa4f07cc60344b073
ChahinezBENF <Chahinez-benfriha27@outlook.com>
2025-05-15 13:36:25 -0400

    17 th commit

--- routes/airport.js
+++ routes/airport.js
@@ -7,17 +7,17 @@ const authorizeRoles = require('../middleware/authorizeRoles');
 
 // Before deployement it works perfectly but after it didnt 
 // Routes for airports with Authen and Autor
-// router.get('/', authenticateToken, airportController.getAllAirports);
-// router.post('/', authenticateToken, authorizeRoles('hr'), airportController.createAirport);
-// router.get('/:id', authenticateToken, airportController.getAirportById);
-// router.put('/:id', authenticateToken, authorizeRoles('hr'), airportController.updateAirport);
-// router.delete('/:id', authenticateToken, authorizeRoles('hr'), airportController.deleteAirport);
+router.get('/', authenticateToken, airportController.getAllAirports);
+router.post('/', authenticateToken, authorizeRoles('hr'), airportController.createAirport);
+router.get('/:id', authenticateToken, airportController.getAirportById);
+router.put('/:id', authenticateToken, authorizeRoles('hr'), airportController.updateAirport);
+router.delete('/:id', authenticateToken, authorizeRoles('hr'), airportController.deleteAirport);
 
 // Routes for airports 
-router.get('/',  airportController.getAllAirports);
-router.post('/',  airportController.createAirport);
-router.get('/:id',  airportController.getAirportById);
-router.put('/:id',  airportController.updateAirport);
-router.delete('/:id',  airportController.deleteAirport);
+// router.get('/',  airportController.getAllAirports);
+// router.post('/',  airportController.createAirport);
+// router.get('/:id',  airportController.getAirportById);
+// router.put('/:id',  airportController.updateAirport);
+// router.delete('/:id',  airportController.deleteAirport);
 
 module.exports = router;
